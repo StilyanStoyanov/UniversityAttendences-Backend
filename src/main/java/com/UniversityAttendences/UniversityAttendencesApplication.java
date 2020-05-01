7d2f94df8b5b3823c7f1374ec475124e277e775d
Stilyan Stoyanov <stilyan.stoyanov@softwaregroup.com>
2020-05-01 20:04:21 +0300

    Project initialize.

---
+++ src/main/java/com/UniversityAttendences/UniversityAttendencesApplication.java
@@ -0,0 +1,14 @@
+package com.UniversityAttendences;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversityAttendencesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniversityAttendencesApplication.class, args);
+		System.out.println("We are up !");
+	}
+
+}
---
+++ src/test/java/com/UniversityAttendences/UniversityAttendencesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.UniversityAttendences;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversityAttendencesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
